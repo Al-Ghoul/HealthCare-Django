eb4b1ee334ee05c957d0a6e0f51d8229abcd7af6
Al-Ghoul <Abdo.alghouul@gmail.com>
2022-12-30 16:31:54 +0200

    Add Tailwind and main app

--- HealthCare/HealthCare/settings.py
+++ HealthCare/HealthCare/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tailwind',
+    'main.apps.MainConfig'
 ]
 
 MIDDLEWARE = [
@@ -121,3 +123,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+TAILWIND_APP_NAME = 'main'
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
+
+NPM_BIN_PATH = 'C:\\Program Files\\nodejs\\npm.cmd'
